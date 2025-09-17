43d5cc441dddd8cddafc134f85abce05a4968fd7
bakyt92 <bakyt92@mail.ru>
2025-09-17 03:10:49 +0200

    update

--- Day1/ex01/format_ft_time.py
+++ Day1/ex01/format_ft_time.py
@@ -1,7 +1,8 @@
-import time 
+import time as time
 
 x = time.time()
-
+date_format = time.strftime("%B %d, %Y")
 print("Seconds since January 1, 1970: ", "{:,.4f}".format(x), " seconds or in scientific notation: ", "{:e}".format(x)) 
+print(date_format)
 
 # https://docs.python.org/3/library/string.html#

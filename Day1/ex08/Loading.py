d92954f91d63f220dc16a45ec2336739c859a539
bakyt92 <bakyt92@mail.ru>
2025-12-28 20:17:05 +0400

    formatting

--- Day1/ex08/Loading.py
+++ Day1/ex08/Loading.py
@@ -11,20 +11,22 @@ def ft_tqdm(lst: range) -> None:
     try: 
         size = os.get_terminal_size().columns
     except OSError:
-        size = 20
-    progress_bar_w = size - 10
+        size = 80
+    progress_bar_w = size - 70
     for x, name in enumerate(lst, start=1):
-        
+        status = int(x / total_elem * progress_bar_w)
         time_used = time.time() - start
         formatted_time_used = time_format(time_used)
         speed = x / time_used if time_used > 0 else 0
         to_finish = (total_elem - x) / speed
         formatted_to_finish = time_format(to_finish)
-        print(f"\r{formatted_time_used} {formatted_to_finish}", end="")
+        progress = f"|{'=' * status:<{progress_bar_w}}"
+        percentage = int (x / total_elem * 100)
+        print(f"\r{percentage}% {progress} | {x}/{total_elem} [{formatted_time_used}<{formatted_to_finish}, {speed:.2f} iter/sec]", end="", flush=True)
         yield name
 
 def main():
-    for x in ft_tqdm(range(0, 5000000)):
+    for x in ft_tqdm(range(0, 500000)):
         pass
 
 if __name__ == "__main__":

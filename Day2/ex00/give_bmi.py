0ac395700a5279348f369a7affb27e4f3d8b0739
bakyt92 <bakyt92@mail.ru>
2025-12-30 22:32:20 +0400

    update

---
+++ Day2/ex00/give_bmi.py
@@ -0,0 +1,13 @@
+
+
+def give_bmi(height: list[int | float], weight: list[int | float]) -> list[int | float]:
+#your code here
+
+def apply_limit(bmi: list[int | float], limit: int) -> list[bool]:
+#your code here
+
+def main():
+# your tests and your error handling
+
+if __name__ == "__main__":
+    main()

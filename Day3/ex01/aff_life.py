02acfb30d15334ab8ab10cdd8942bb69aeed3b0a
bakyt92 <bakyt92@mail.ru>
2026-02-15 21:32:18 +0400

    update

--- Day3/ex01/aff_life.py
+++ Day3/ex01/aff_life.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import pandas as pd 
+from matplotlib.ticker import MultipleLocator
 from load_csv import load
 import sys
 
@@ -8,8 +9,19 @@ def ft_visualize(Dataset):
     try:
         Dataset = Dataset.set_index('country')
         selected = Dataset.loc['France']
-        print(Dataset)
-        print(selected)
+        #print(Dataset)
+        #print(selected)
+        years = selected.index.astype(int)
+        values = selected.values.astype(float)
+        fig, ax = plt.subplots()
+        ax.plot(years, values)
+        ax.set_xticks(range(1800, 2101, 40))  
+        # ax.set_xticks(years)
+        # tick_labels = [year if i % 40 == 0 else '' for i, year in enumerate(years)]
+        # ax.set_xticklabels(tick_labels)
+        ax.set_xlabel('Year')
+        ax.set_ylabel('Life expectancy')
+        plt.show()
     except Exception as e:
         print(e)
         sys.exit(1)

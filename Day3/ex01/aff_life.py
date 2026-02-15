c4e96e0c3de5533cc54c082d7596a5bcdbd68aa2
bakyt92 <bakyt92@mail.ru>
2026-02-15 21:37:54 +0400

    update_norminette ex01

--- Day3/ex01/aff_life.py
+++ Day3/ex01/aff_life.py
@@ -1,5 +1,4 @@
 import matplotlib.pyplot as plt
-from matplotlib.ticker import MultipleLocator
 from load_csv import load
 import sys
 
@@ -15,10 +14,10 @@ def ft_visualize(Dataset):
         values = selected.values.astype(float)
         fig, ax = plt.subplots()
         ax.plot(years, values)
-        ax.set_xticks(range(1800, 2101, 40))  
+        ax.set_xticks(range(1800, 2101, 40))
         ax.set_xlabel('Year')
         ax.set_ylabel('Life expectancy')
-        ax.set_title('France Life Expectancy') 
+        ax.set_title('France Life Expectancy')
         plt.show()
     except Exception as e:
         print(e)

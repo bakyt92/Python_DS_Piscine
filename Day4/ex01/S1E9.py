78f7ab6f37a85575279defa6d34f99ff0ba661e1
bakyt92 <bakyt92@mail.ru>
2026-02-21 22:47:55 +0400

    ex01 and 00

---
+++ Day4/ex01/S1E9.py
@@ -0,0 +1,31 @@
+from abc import ABC, abstractmethod
+
+
+class Character(ABC):
+    """Class of main character in this application"""
+
+    def __init__(self, first_name: str, is_alive: bool = True):
+        """ Creates Character """
+        self.first_name = first_name
+        self.is_alive = is_alive
+
+    @abstractmethod
+    def die(self):
+        """Abstract (mandatory method for
+        all character class instances)
+        Sets value is alive to False"""
+        self.is_alive = False
+        return
+
+
+class Stark(Character):
+    """STARK is sub-class of Character class"""
+    def __init__(self, first_name: str, is_alive: bool = True):
+        """ Creates STARK Character, subclass of Character """
+        super().__init__(self, first_name, is_alive)
+        return
+
+    def die(self):
+        """Kill the Stark by setting is_alive to False."""
+        self.is_alive = False
+        return

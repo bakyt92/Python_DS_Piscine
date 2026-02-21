12c4629c30acb34db55d72c8bf020d1cea94dace
bakyt92 <bakyt92@mail.ru>
2026-02-21 23:11:32 +0400

    update

---
+++ Day4/ex02/DiamondTrap.py
@@ -0,0 +1,32 @@
+from S1E7 import Baratheon, Lannister
+
+
+class King(Baratheon, Lannister):
+    """ Representing KING class """
+    def __init__(self, first_name: str, is_alive: bool = True):
+        """ Creates STARK Character, subclass of Character """
+        super().__init__(first_name, is_alive)
+        return
+
+    def set_eyes(self, _eyes: str):
+        """ Set method to set color of eyes """
+        self.eyes = _eyes
+
+    def set_hairs(self, _hairs: str):
+        """ Set method to set color of eyes """
+        self.hairs = _hairs
+
+    def get_hairs(self):
+        """ Getter method for hairs """
+        return(self.hairs)
+
+    def get_eyes(self):
+        """ Getter method for eyes """
+        return(self.eyes)
+
+    def die(self):
+        """Abstract (mandatory method for
+        all character class instances)
+        Sets value is alive to False"""
+        self.is_alive = False
+        return
